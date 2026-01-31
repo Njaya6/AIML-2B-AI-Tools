bcd65f5e5634435543b2a1d0ee0a9ce71c08ec6e
Njaya6 <futurebinga@gmail.com>
2026-02-01 03:28:27 +0530

    done

---
+++ loop.py
@@ -0,0 +1,35 @@
+# display even numbers from 0 to n
+n= int(iput("enter range: "))
+for i in range(0,n):
+    if i%2 =0 :
+    print(i end=" ")
+
+# Break function
+ my_list= [56, 5 8 ,90, 7 ,48]
+ x=56
+ for i in my_list:
+    if i =x :
+        print("Number found at index ",i)
+        break()
+    else :
+    print("Number not found!")
+
+# continue function
+  for i in my_list:
+    if my_list[i]> my_list[i+1] :
+        continue()
+        print(my_list[i] end= "\t") 
+
+# A program in pythoh to check palindrome
+y = int (iput("enter a number: "))
+rev=0
+while y>0 :
+    rem = y % 10
+    rev = rev*10 + rem
+    y=int(y/10)
+if rev ==y :
+    print("f{y} is palindrome")
+else : print("f{y} is not paliindrome")
+       
+
+   
