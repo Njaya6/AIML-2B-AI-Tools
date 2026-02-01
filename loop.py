1b0beb8c700f9958a1c73b10f2b4ed83a5e44a68
Njaya6 <cannynjayaadmire@gmail.com>
2026-02-02 01:40:44 +0530

    Correct syntax errors in loop.py
    Fix syntax errors in loop.py for even number display, break, and continue functions.

--- loop.py
+++ loop.py
@@ -1,8 +1,7 @@
 # display even numbers from 0 to n
 n= int(input("enter range: "))
 for i in range(0,n):
-    if i%2 =0 :
-    print(i end=" ")
+    if i%2 =0 : print(i, end=" ")
 
 # Break function
  my_list= [56, 5 8 ,90, 7 ,48]
@@ -12,13 +11,13 @@ for i in range(0,n):
         print("Number found at index ",i)
         break():
     else :
-    print("Number not found!")
+      print("Number not found!")
 
 # continue function
   for i in my_list:
     if my_list[i]> my_list[i+1] :
         continue():
-        print(my_list[i] end= "\t") 
+    print(my_list[i] end= "\t") 
 
 # A program in pythoh to check palindrome
 y = int (input("enter a number: "))
